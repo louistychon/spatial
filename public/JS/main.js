49af640a3515423ecb915c43b41b549bc412a971
louistychon <louis.tychon1@gmail.com>
2022-04-18 15:29:58 +0200

    button darkmode finalized

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,10 +1,21 @@
 const two = document.querySelector('#two');
 const footer = document.querySelector('footer');
+var active = false;
 
 function toggledark() {
-        document.body.style.backgroundColor = "black";
-        document.body.style.color = "white";
-        two.style.backgroundColor = "black";
-        footer.style.color = "white";
-        footer.style.backgroundColor = "black";
+        if (active == false) {
+                document.body.style.backgroundColor = "black";
+                document.body.style.color = "white";
+                two.style.backgroundColor = "black";
+                footer.style.color = "white";
+                footer.style.backgroundColor = "black";
+                active = true;
+        } else {
+                document.body.style.backgroundColor = "white";
+                document.body.style.color = "black";
+                two.style.backgroundColor = "white";
+                footer.style.color = "black";
+                footer.style.backgroundColor = "white";
+                active = false;
+        }
 }
